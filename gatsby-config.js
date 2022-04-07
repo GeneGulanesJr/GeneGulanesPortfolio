0b98babbc686fb9c80e6bca9eaa9047ffa924ede
GeneGulanesJr <ggulanes_180000000405@uic.edu.ph>
2022-04-07 20:52:12 +0800

    Added Gtags

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,24 @@ module.exports = {
     twitterUsername: '@mpgh5574',
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-P4PMB3M139", // Google Analytics / GA
+
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
